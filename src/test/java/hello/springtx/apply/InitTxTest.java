7c346c867f71136e55cabd415d3b8eaa50e303b1
jurio5 <sia5492@naver.com>
2025-06-03 02:39:31 +0900

    [feat] 트랜잭션 적용 순서

---
+++ src/test/java/hello/springtx/apply/InitTxTest.java
@@ -0,0 +1,54 @@
+package hello.springtx.apply;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class InitTxTest {
+
+    @Autowired
+    Hello hello;
+
+    @Test
+    void go() {
+        // 초기화 코드는 스프링이 초기화 시점에 호출한다.
+        // PostConstruct 즉, 초기화 코드가 먼저 호출되고, 그 다음 트랜잭션 AOP 가 적용되기 때문
+        // 이벤트 리스너를 통해 애플리케이션이 구동된 뒤 호출되도록 하면 정상 작동
+    }
+
+    @TestConfiguration
+    static class InitTestConfig {
+
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello {
+
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruct tx active={}", isActive);
+        }
+
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
+        public void initV2() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello initApplicationReadyEvent tx active={}", isActive);
+        }
+    }
+}
